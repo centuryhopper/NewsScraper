0bf126b583ad01c134afe18831893eedf2a60bf5
Leo Zhang <leotheasianlion@gmail.com>
2022-12-26 12:39:23 -0500

    update

--- linux_tools.py
+++ linux_tools.py
@@ -1,3 +1,6 @@
-AUTOMATE_TEXTING_PATH = '/home/leo_zhang/Documents/GitHub/automate_texting/'
+import os, pwd
+
+
+AUTOMATE_TEXTING_PATH = f'/home/{pwd.getpwuid(os.getuid()).pw_name}/Documents/GitHub/automate_texting/'
 LOG_PATH='/tmp/'
 
