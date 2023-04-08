76cc6fcbd19d5f23c200ea37a37fedfd2ed4c2fb
Leo Zhang <leotheasianlion@gmail.com>
2023-04-07 20:29:00 -0400

    update

--- linux_tools.py
+++ linux_tools.py
@@ -5,6 +5,7 @@ import subprocess
 AUTOMATE_TEXTING_PATH = f'/home/{pwd.getpwuid(os.getuid()).pw_name}/Documents/GitHub/automate_texting/'
 LOG_PATH='/tmp/'
 
+AUTOMATE_EMAIL_PATH = f'/home/{pwd.getpwuid(os.getuid()).pw_name}/Documents/GitHub/Tools/EmailTool/'
 
 WIFI_CHECK = subprocess.run(['curl', '-Is', 'https://www.google.com'], capture_output=True, text=True).stdout
 IS_CONNECTED_TO_NETWORK = 'HTTP/2 200' in WIFI_CHECK
